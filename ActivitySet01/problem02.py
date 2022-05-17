5dd4718fddd96723d0354349348c30a437dde1fb
Shashank B N <97089480+Shashank100200@users.noreply.github.com>
2022-05-17 14:49:34 +0000

    completed with P3

--- ActivitySet01/problem02.py
+++ ActivitySet01/problem02.py
@@ -1,3 +1,4 @@
 # Why Program
 
 print("hello world")
+#print(float(10)+25)
--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,8 @@
 # Variables, Expressions & Statements
 
-hrs = float(input("Enter hours? "))
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter the rate:"))
+pay = hrs*rate
+print("Pay:",pay)
+
+
